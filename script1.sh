514533a9892c15bcf30ea8839b513e9e7bb8182d
shellcripts <siddhantgunjal897@gmail.com>
2024-08-21 22:26:04 +0530

    new one

--- script1.sh
+++ script1.sh
@@ -1,3 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "we are learning devops"
+echo "we are learning devsecops"
+echo "good night oll of you"
