3e9502c15393cc9ccaf356f08d933a431b7ba9ea
shellcripts <siddhantgunjal897@gmail.com>
2024-08-24 17:17:12 +0530

    INC4515254 is new changes

--- script1.sh
+++ script1.sh
@@ -1,7 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
-echo "we are learning devops and git"
-echo "we are complete aws cloud"
-echo "hello team good night all of you"
-echo "good night"
+
 
